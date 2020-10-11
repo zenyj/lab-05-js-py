36b321c361f7ae371939de1ecf83a0735947ddc9
zenyj <70486936+zenyj@users.noreply.github.com>
2020-10-10 21:55:37 -0400

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,5 @@
+# 1. create a variable to store the name of the user
+# input() is a FUNCTION!
+username = input("So, umm, like, what's your name? ")
+#concatenate strings, print result to the terminal
+print("hi " + str(username) + "!") 
---
+++ madlibs.py
@@ -0,0 +1,15 @@
+noun = input("Give me a noun:")
+adjective = input("How about an adjective?:")
+adverb = input("Can I get an adverb?:")
+verb = input("How about an verb?:")
+    
+print("On a tropical island " + str(noun) + ",")
+print(" Underneath a " + str(adjective) + " lava moon. ")
+print(" hangin " + str(adverb) + " with the hula dancers ")
+print(str(verb) + " questions cause they all got the answers. ")
+    
+    
+    
+    
+    
+    
